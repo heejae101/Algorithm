5b0612c8b2b87e4fc71fb169829c4dcf6eee1cf8
heejae101 <heejae0629@naver.com>
2023-09-08 23:58:07 +0900

    modified LRU

--- LRU.java
+++ LRU.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class LRU {
+
+    public int[] solution(int arrLen, int[] arr){
+        return arr;
+    }
+    public static void main(String[] args) {
+        LRU a = new LRU();
+        Scanner sc = new Scanner(System.in);
+        int arrLen = sc.nextInt();
+    }
 }
