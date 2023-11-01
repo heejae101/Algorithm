5455ac645e8f7d5f9dae8a585644a16d7692b17a
heejae0629 <heejae0629@naver.com>
2023-11-01 23:48:26 +0900

    REFACTOR : 2023.11.01

--- section4/Solution4.java
+++ section4/Solution4.java
@@ -6,22 +6,25 @@ import java.util.Scanner;
 public class Solution4 {
     public int solution(String s,String t){
         int result = 0;
-        HashMap<Character, Integer> map = new HashMap<>();
-        for(int i=0; i<s.length(); i++){
-            int cnt = 0;
-            int lt = i;
-            int rt = t.length()-1+i;
-            for(int j = lt; j<=rt; j++){
-                map.put(s.charAt(j),1);
-            }
-            for(int j=0; j<; j++){
-                if(map.get((t.charAt(j))) == 1){
-
-                }
-            }
-
+        // s 문자열
+        HashMap<Character,Integer> sMap = new HashMap<>();
+        // t 문자열
+        HashMap<Character,Integer> tMap = new HashMap<>();
+        // t문자열에 있는 문자들 넣기
+        for(char x : t.toCharArray()){
+            tMap.put(x,tMap.getOrDefault(x,0)+1);
         }
-
+        // s 문자열에서 t길이에 -1 한만큼 s문자열 넣고 시작
+        for(int i=0; i<t.length()-1; i++){
+            sMap.put(s.charAt(i),sMap.getOrDefault(s.charAt(i),0)+1);
+        }
+        for(int i=0; i<s.length()-t.length()+1; i++){
+            int rt = t.length()-1+i;
+            sMap.put(s.charAt(rt),sMap.getOrDefault(s.charAt(rt),0)+1);
+            if(sMap.equals(tMap)) result++;
+            if(sMap.get(s.charAt(i))>1) sMap.put(s.charAt(i),sMap.get(s.charAt(i))-1);
+            else sMap.remove(s.charAt(i));
+        }
         return result;
     }
     public static void main(String[] args) {
