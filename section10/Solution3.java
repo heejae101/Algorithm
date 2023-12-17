404e778eae0fb26448cd7d8fbb6e472c9d6c3e1a
heejae0629 <heejae0629@naver.com>
2023-12-17 18:21:13 +0900

    ADD : 2023.12.17

--- section10/Solution3.java
+++ section10/Solution3.java
@@ -9,7 +9,8 @@ public class Solution3 {
     public int solution(int n){
         int answer = 0;
         dy[1] = 1;
-        for(int i=2; i<n; i++){
+//        answer = dy[1]; 길이가 2 이하인 배열은 실행이 안되므로
+        for(int i=1; i<n; i++){
             int max = 0;
             for(int j=i-1; j>=0; j--){
                 if(arr[j]<arr[i] && dy[j]>max) max = dy[j];
